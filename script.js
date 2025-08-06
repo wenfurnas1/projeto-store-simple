46938149341a79921bcc2cd612afce24e88ab89e
wenfuras1 <wenfurnas@alu.ufc.br>
2025-08-06 19:06:20 -0300

    primeira versão store, nada ainda foi adicionado

---
+++ script.js
@@ -0,0 +1,9 @@
+function rolarParaEsquerda() {
+    const a = document.querySelector(".vitrine");
+    a.scrollLeft -= 405;
+}
+
+function rolarParaDireita() {
+    const a = document.querySelector(".vitrine");
+    a.scrollLeft += 405;
+}
