fb0ddab5501e328121d319f6bd0697f8a0df78fc
wenfuras1 <wenfurnas@alu.ufc.br>
2025-08-07 14:20:09 -0300

    inicio da criacao de carrinho

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ function rolarParaDireita() {
     const a = document.querySelector(".vitrine");
     a.scrollLeft += 950;
 }
+
+const carro = document.querySelector("#carro");
+const carrinhoflutuante = document.querySelector("#carrinhoflutuante");
+
+carro.addEventListener('click', function(mostrarcarro) {
+    if (carrinhoflutuante.style.display === "none" || carrinhoflutuante.style.display === "") {
+        carrinhoflutuante.style.display = "block";
+    } else {
+        carrinhoflutuante.style.display = "none";
+    }
+})
