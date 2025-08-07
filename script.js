97b62d55c41ea83ea15bc8e01adb6a9dafc6c8c2
wenfuras1 <wenfurnas@alu.ufc.br>
2025-08-07 16:45:20 -0300

    carrinho pré funcional adicionado

--- script.js
+++ script.js
@@ -11,10 +11,40 @@ function rolarParaDireita() {
 const carro = document.querySelector("#carro");
 const carrinhoflutuante = document.querySelector("#carrinhoflutuante");
 
-carro.addEventListener('click', function(mostrarcarro) {
+carro.addEventListener('click', function() {
     if (carrinhoflutuante.style.display === "none" || carrinhoflutuante.style.display === "") {
         carrinhoflutuante.style.display = "block";
     } else {
         carrinhoflutuante.style.display = "none";
     }
-})
+});
+window.addEventListener("DOMContentLoaded", () => {
+
+    class ItemCarrinho {
+        constructor(nome, preco) {
+            this.nome = nome;
+            this.preco = preco;
+        }
+    }
+
+
+    const vetorCarrinho = [];
+
+    const botoesCarrinho = document.querySelectorAll('.carrinho');
+    const flutuant = document.querySelector('#carrinhoflutuante');
+
+    botoesCarrinho.forEach(function(botao) {
+        botao.addEventListener('click', function() {
+            const card = botao.closest('.card');
+            const nomeProduto = card.querySelector('h2').innerText;
+            const precoProduto = card.querySelector('.price').innerText;
+
+            const item = new ItemCarrinho(nomeProduto, precoProduto);
+            vetorCarrinho.push(item);
+            flutuant.innerHTML = vetorCarrinho.map(produto => `
+                <p>${produto.nome} - ${produto.preco}</p>
+            `).join('');
+        });
+    });
+
+});
